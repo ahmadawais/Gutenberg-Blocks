1517248d7b96c1446a7bf67d8a4938e7ca2c081a
George Stephanis <georgestephanis@automattic.com>
2017-07-03 13:54:38 -0400

    Change the source file from .js to .jsx

---
+++ blocks/tweet/block.jsx
@@ -0,0 +1,24 @@
+/**
+ * BLOCK: Tweet
+ *
+ * Tweet block JS.
+ *
+ * @since  1.0.0
+ */
+wp.blocks.registerBlockType( 'gb/tweet', {
+    title: 'Tweet',
+    icon: 'twitter',
+    category: 'common',
+
+    attributes: {
+        text: 'Tweet Text',
+    },
+
+    edit: function( props ) {
+        return <a href='https://twitter.com/home?status=Custom%20Tweet!'>Click To Tweet!</a>
+    },
+
+    save: function( props ) {
+        return <a href='https://twitter.com/home?status=Custom%20Tweet!'>Click To Tweet!</a>
+    }
+} );
